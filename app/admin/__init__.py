4803a406587d383770b7af846a161e8ac629ff05
Arka <itsarkanayan@gmail.com>
2016-04-07 09:31:17 +0530

    Add .travis.yml

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 admin_bp = Blueprint('admin_bp', __name__,
                      template_folder='templates',
-                     static_folder="static")
+                     static_folder="static",
+                     subdomain="admin")
 
 from . import views
