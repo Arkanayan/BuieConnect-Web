ccf246c7391dbbe7ee65b9b78220ccb50b5f4cb8
Arka <itsarkanayan@gmail.com>
2016-04-09 20:17:32 +0530

    Add dev domain, admin interface now registers and logins

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 admin_bp = Blueprint('admin_bp', __name__,
                      template_folder='templates',
-                     static_folder="static")
+                     static_folder="static",
+                     url_prefix="/admin")
 
 from . import views
